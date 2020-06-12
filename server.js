9fff58723449896538aed2ae915ee26f84b7ec4a
Claudia <sydeng102@gmail.com>
2020-06-12 20:16:36 +0800

    updating the connection

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const image = require('./controllers/image');
 const db = knex ({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'Claudia',
-      password : '',
-      database : 'smart-brain'
-    }
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+          }
+        }
   });
 
 const app = express();
