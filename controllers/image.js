7d74e5c9ccb93c47aca4ac58c950234639bd2fda
Claudia-teng <sydeng102@gmail.com>
2022-02-19 17:02:16 +0800

    change err

--- controllers/image.js
+++ controllers/image.js
@@ -10,7 +10,7 @@ const handleApiCall = (req, res) => {
         .then(data => {
             res.json(data);
         })
-        .catch(err => res.status(400).json('enable to work with API'))
+        .catch(err => res.status(400).json('enable to work with API!'))
 }
   
 
